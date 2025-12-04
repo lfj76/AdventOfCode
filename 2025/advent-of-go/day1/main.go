b7bf2ac04bc69920e7ef19a4508ebb58929bdb51
Lucas Jenkins <lucas@florisoft.nl>
2025-12-04 13:19:04 +0100

    day 1 problem 2

--- 2025/advent-of-go/day1/main.go
+++ 2025/advent-of-go/day1/main.go
@@ -27,10 +27,50 @@ func main() {
 			inputs = append(inputs, num)
 		}
 	}
-	res := solve(inputs)
+	res := solve2(inputs)
 	fmt.Println("Result:", res)
 }
 
-func solve(inputs []int) int {
-	return 0
+func solve1(inputs []int) int {
+	dial := 50
+	code := 0
+	for i := range inputs {
+		dial = (dial + inputs[i]) % 100
+		if dial == 0 {
+			code += 1
+		}
+	}
+	return code
+}
+
+func solve2(inputs []int) int {
+	dial := 50
+	code := 0
+	// inputs := []int{-68, -30, 48, -5, 60, -55, -1, -99, 14, -82}
+
+	for i := range inputs {
+		newNum := dial + inputs[i]
+		// fmt.Println("change: ", inputs[i], " result: ", newNum)
+		if newNum <= 0 && dial != 0 {
+			code += 1
+			//	fmt.Println("newNum <= 0 and dial !=0. -> new code: ", code)
+		}
+		code += abs(newNum) / 100
+		// fmt.Println("new code: ", code)
+
+		dial = mod(dial+inputs[i], 100)
+		// fmt.Println("new dial: ", dial)
+	}
+	return code
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func mod(x, y int) int {
+	return (x%y + y) % y
 }
